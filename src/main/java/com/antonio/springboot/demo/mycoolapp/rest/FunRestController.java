8dfb31b2ee1221ca6780a7da1eac486e6a136898
Antonio Espinoza <a0e0bhj@walmart.com>
2025-05-23 12:19:09 -0400

    Add hello world endpoint to FunRestController

--- src/main/java/com/antonio/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/antonio/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,8 +1,15 @@
 package com.antonio.springboot.demo.mycoolapp.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world";
+    }
+
 }
 
