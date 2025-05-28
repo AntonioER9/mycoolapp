ec3c9737991b221b80d2b5099d295ba6c47f1313
Antonio Espinoza <a0e0bhj@walmart.com>
2025-05-28 17:06:48 -0400

    add some properties

--- src/main/java/com/antonio/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/antonio/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,22 @@
 package com.antonio.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
@@ -12,4 +24,3 @@ public class FunRestController {
     }
 
 }
-
